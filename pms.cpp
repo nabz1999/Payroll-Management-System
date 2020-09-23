2508c863df496ad4a66dcde13e782e8ae04093b9
Nabeel <qadirnabeel16@gmail.com>
2020-09-23 12:20:52 +0530

    Progress on graphics

--- pms.cpp
+++ pms.cpp
@@ -6,3 +6,23 @@
 #include<dos.h>
 #include<iomanip.h>
 #include<process.h>
+
+
+void dev()
+{
+ clrscr();
+  for(int x=50;x>=20;x--)
+     {
+       textcolor(WHITE+LIGHTGRAY);
+       delay(160);
+       gotoxy(x,12);
+       cputs("  P R O J E C T   ");
+       gotoxy(x,14);
+       cputs("  D E V E L O P E D  B Y  :  ");
+       gotoxy(x,16);
+       cputs("N A B E E L");
+      }
+ gotoxy(x,24);
+ cputs("PRESS ANY KEY TO CONTINUE");
+ getch();
+}
