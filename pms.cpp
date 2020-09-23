868bcdcfd2c3791fc7e60d68ca85207b7658e357
Nabeel <qadirnabeel16@gmail.com>
2020-09-23 16:31:20 +0530

    Created Main

--- pms.cpp
+++ pms.cpp
@@ -26,3 +26,23 @@ void dev()
  cputs("PRESS ANY KEY TO CONTINUE");
  getch();
 }
+
+void main()
+ {
+    clrscr();
+    dev();
+    int gdriver=DETECT,gmode,errorcode;
+    initgraph(&gdriver,&gmode,"c:\\turboc3\\bgi");
+    errorcode=graphresult();
+    if(errorcode!=grOk)
+      {
+          cout<<grapherrormsg(errorcode)<<"\n";
+          getch();
+        }
+    settextstyle(1,0,7);
+    outtextxy(75,1," W E L C O M E  ");
+    outtextxy(275,150,"T O     ");
+    outtextxy(65,300," P A Y R O L L    M A N A G E M E N T   S O F T W A R E ");
+    outtextxy(9,400,"Press any key to continue ");
+getch();
+}
